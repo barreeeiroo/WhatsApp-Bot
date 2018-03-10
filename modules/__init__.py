0c3b6cf210ac6a5f48c0e320cda3dfa0add22f22
Diego Barreiro <diego.barreiro.perez@gmail.com>
2018-03-10 23:29:40 +0100

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,5 +3,6 @@
 from modules.hihelp import hihelp
 from modules import pokedex
 from modules import poker
+from modules import roulette
 from modules.poll import poll
 from modules.poll2 import poll2
